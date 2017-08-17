d72180f966c0d60742f5ac4b90964fc417f39282
John Kastner <john.h.kastner@gmail.com>
2017-08-17 12:03:28 -0400

    Fixed test

--- tests/test_hes.py
+++ tests/test_hes.py
@@ -1,36 +1,36 @@
 """Test for the hes module"""
-#from unittest import TestCase
 import unittest
 import zeep
 
 from hes import hes
 
-class HesTest(unittest.TestCase):
-    
-    def setUp(self):
-        self.building_info={'user_key':'ce4cdc28710349a1bbb4b7a047b65837','building_id':'142543'}
-        
-    def test_client_connectivity(self):
-        client = hes.connect_client()
-        self.assertIsInstance(client, zeep.Client)
-        
-    def test_succesful_completion(self):
-        score = hes.hes_helix(self.building_info)
-        self.assertEqual(score['base_score'], 6)
 
-#    def test_bad_key(self):
-#        bad_info = self.building_info
-#        bad_info.update({'user_key':'abc123'})
-#        response = hes.hes_helix(bad_info)
-    
+class HesTest(unittest.TestCase):
+
+    def setUp(self):
+        # This information is likely to change or become outdated as the hes
+        # api updates or acount information is changed. If a lot of tests
+        # start failing, make sure this is up to date.
+        self.user_name = 'TST-HELIX'
+        self.password = 'helix123'
+        self.user_key = '520df908c6cb4bea8c14691ee95aff88'
+        self.building_id = '142860'
+
+        self.hes_client = hes.HesHelix(hes.CLIENT_URL, self.user_name, self.password, self.user_key)
+
+    def test_client_connectivity(self):
+        self.assertIsInstance(self.hes_client.client, zeep.Client)
+
+    def test_succesful_completion(self):
+        score = self.hes_client.query_hes(self.building_id)
+        self.assertEqual(score['base_score'], 4)
+
     def test_fail_bad_bulding_id(self):
         from zeep.exceptions import Fault
 
-        self.building_info['building_id'] = '999999'
-
         try:
-            hes.test_client(self.building_info)
-        except Fault as f:
-            self.assertEqual(f.message,"No building found for building_id #999999")
+            self.hes_client.query_hes(142861)
+        except Fault:
+            self.assertTrue(True)
         else:
             self.assertTrue(False)
